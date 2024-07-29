a75f32bf82b4b1cea4bc131c7001f2fbb4d714e1
Dima Zemnitski <g.fredex@gmail.com>
2024-07-29 21:36:38 +0300

    feat: added working  modules of my project

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+async function compileSass() {
+    const autoprefixer = (await import('gulp-autoprefixer')).default;
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css'));
+}
+
+function watch() {
+    gulp.watch('./scss/**/*.scss', compileSass);
+}
+
+exports.compileSass = compileSass;
+exports.watch = watch;
